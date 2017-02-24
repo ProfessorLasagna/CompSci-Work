3dc18bedc85db50a4fca389de7ab36535fa3ac8b
999104001 <999104001@W870-2130.edu.cbe.ab.ca>
2017-02-24 13:07:24 -0700

    New stuff

--- ColourConverter/src/util/converter/ColourConverter.java
+++ ColourConverter/src/util/converter/ColourConverter.java
@@ -59,8 +59,221 @@ public class ColourConverter extends EasyApp{//
 	
 	 public void convertCode(String hexcode){
 		 
+		 String r1, r2 , g1, g2, b1, b2;
+		 int red = 0, green = 0, blue = 0;
 		 
+		 r1 = hexcode.substring(1, 2);
+		 r2 = hexcode.substring(2, 3);
+		 g1 = hexcode.substring(3, 4);
+		 g2 = hexcode.substring(5, 6);
+		 b1 = hexcode.substring(6, 7);
+		 b2 = hexcode.substring(7);
 		 
+		red = hextodec(r1, r2);
+		green = hextodec(g1, g2);
+		blue = hextodec(b1, b2);
+		
+		
+		
 	 }
+		 
+	 
+	 
+	 public int hextodec(String one, String two){
+		
+		 int sixteens = 0, ones = 0, total;
+		 
+		 if(one.startsWith("1")){
+			 
+			 sixteens = 1 * 16;
+			 
+		 }
+		 
+		 else if(one.startsWith("2")){
+			 
+			 sixteens = 2 * 16;
+			 
+		 }
+		 
+		 else if(one.startsWith("3")){
+			 
+			 sixteens = 3 * 16;
+			 
+		 }
+		 
+		 else if(one.startsWith("4")){
+			 
+			 sixteens = 4 * 16;
+			 
+		 }
+		 
+		 else if(one.startsWith("5")){
+			 
+			 sixteens = 5 * 16;
+			 
+		 }
+		 
+		 else if(one.startsWith("6")){
+			 
+			 sixteens = 6 * 16;
+			 
+		 }
+		 
+		 else if(one.startsWith("7")){
+			 
+			 sixteens = 7 * 16;
+			 
+		 }
+		 
+		 else if(one.startsWith("8")){
+			 
+			 sixteens = 8 * 16;
+			 
+		 }
+		 
+		 else if(one.startsWith("9")){
+			 
+			 sixteens = 9 * 16;
+			 
+		 }
+		 
+		 else if(one.startsWith("A")){
+			 
+			 sixteens = 10 * 16;
+			 
+		 }
+		 
+		 else if(one.startsWith("B")){
+			 
+			 sixteens = 11 * 16;
+			 
+		 }
+		 
+		 else if(one.startsWith("C")){
+			 
+			 sixteens = 12 * 16;
+			 
+		 }
+		 
+		 else if(one.startsWith("D")){
+			 
+			 sixteens = 13 * 16; 
+			 
+		 }
+		 
+		 else if(one.startsWith("E")){
+			 
+			 sixteens = 14 * 16;
+			 
+		 }
+		 
+		 else if(one.startsWith("F")){
+			 
+			 sixteens = 15 * 16;
+			 
+		 }
+		 
+		 if(two.startsWith("0")){
+			 
+			 ones = 0;
+			 
+		 }
+		 
+		 else if(two.startsWith("1")){
+			 
+			 ones = 1;
+			 
+		 }
+		 
+		 else if(two.startsWith("2")){
+			 
+			 ones = 2;
+			 
+		 }
+		 
+		 else if(two.startsWith("3")){
+			 
+			 ones = 3;
+			 
+		 }
+		 
+		 else if(two.startsWith("4")){
+			 
+			 ones = 4;
+			 
+		 }
+		 
+		 else if(two.startsWith("5")){
+			 
+			 ones = 5;
+			 
+		 }
+		 
+		 else if(two.startsWith("6")){
+			 
+			 ones = 6;
+			 
+		 }
+		 
+		 else if(two.startsWith("7")){
+			 
+			 ones = 7;
+			 
+		 }
+		 
+		 else if(two.startsWith("8")){
+			 
+			 ones = 8;
+			 
+		 }
+		 
+		 else if(two.startsWith("9")){
+			 
+			 ones = 9;
+			 
+		 }
+		 
+		 else if(two.startsWith("A")){
+			 
+			 ones = 10;
+			 
+		 }
+		 
+		 else if(two.startsWith("B")){
+			 
+			 ones = 11;
+			 
+		 }
+		 
+		 else if(two.startsWith("C")){
+			 
+			 ones = 12;
+			 
+		 }
+		 
+		 else if(two.startsWith("D")){
+			 
+			 ones = 13; 
+			 
+		 }
+		 
+		 else if(two.startsWith("E")){
+			 
+			 ones = 14;
+			 
+		 }
+		 
+		 else if(two.startsWith("F")){
+			 
+			 ones = 15;
+			 
+		 }
+		 
+		 total = ones + sixteens;
+		 
+		 return(total);
+		
+	 }
+	 
 }
 
