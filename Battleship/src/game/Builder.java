b2acb174fb7326602d2da27f042a171943a8b976
999104001 <999104001@W870-2130.edu.cbe.ab.ca>
2017-04-28 13:04:18 -0600

    jffff

--- Battleship/src/game/Builder.java
+++ Battleship/src/game/Builder.java
@@ -1,267 +1,287 @@
 package game;
 
 public class Builder {
-	
+
 	public static int[] bb = new int[5];
-	public static int[] ca = new int[4];
-	public static int[] dd = new int[3];
-	public static int[] cl = new int[3];
-	public static int[] ss = new int[2];
+	public static int[] ca = new int[5];
+	public static int[] dd = new int[5];
+	public static int[] cl = new int[5];
+	public static int[] ss = new int[5];
 	public static int[] vb = new int[5];
+	public static int[] va = new int[5];
+	public static int[] vc = new int[5];
+	public static int[] vd = new int[5];
+	public static int[] ve = new int[5];
 
 	public Builder() {
-		
+
 		for(int a = 0; a < 5; a++){
-			
-			bb[a] = -5;
-			
+
+			bb[a] = -2057;
+
 		}
-		
-		for(int a = 0; a < 4; a++){
-			
-			ca[a] = -5;
-			
+
+		for(int a = 0; a < 5; a++){
+
+			ca[a] = -2058;
+
 		}
-		
-		for(int a = 0; a < 3; a++){
-			
-			dd[a] = -5;
-			
+
+		for(int a = 0; a < 5; a++){
+
+			dd[a] = -2059;
+
 		}
-		
-		for(int a = 0; a < 3; a++){
-			
-			cl[a] = -5;
-			
+
+		for(int a = 0; a < 5; a++){
+
+			cl[a] = -2060;
+
 		}
-		
-		for(int a = 0; a < 2; a++){
-			
-			ss[a] = -5;
-			
+
+		for(int a = 0; a < 5; a++){
+
+			ss[a] = -2061;
+
 		}
-		
+
 	}
-	
+
 	public boolean checkOverlap(int loc, int dir, int boat){
-		
+
 		findCoords(loc, dir, boat);
-		
+
 		if(boat == 4){
-			
+
 			boat = 2;
 			vb = ss;
-			
+			va = dd;
+			vc = cl;
+			vd = bb;
+			ve = ca;
+
 		}else if(boat == 2){
-			
+
 			boat = 3;
 			vb = dd;
-			
+			va = ss;
+			vc = cl;
+			vd = bb;
+			ve = ca;
+
 		}else if(boat == 3){
-			
+
 			boat = 3;
 			vb = cl;
-			
+			va = dd;
+			vc = ss;
+			vd = bb;
+			ve = ca;
+
 		}else if(boat == 1){
-			
+
 			boat = 4;
 			vb = ca;
-			
+			va = dd;
+			vc = cl;
+			vd = bb;
+			ve = ss;
+
 		}else if(boat == 0){
-			
+
 			boat = 5;
 			vb = bb;
-			
+			va = dd;
+			vc = cl;
+			vd = ss;
+			ve = ca;
+
 		}
-		
+
 		if(loc < 100 && loc >= 0){
-			
-			for(int a = 0; a < boat; a++){
-				
-//				if(bb[0] == vb[a] || bb[1] == vb[a] ||bb[2] == vb[a] ||bb[3] == vb[a] ||bb[4] == vb[a] || 
-//						ca[0] == vb[a] || ca[1] == vb[a] || ca[2] == vb[a] || ca[3] == vb[a] || 
-//						dd[0] == vb[a] || dd[1] == vb[a] || dd[2] == vb[a] ||
-//						cl[0] == vb[a] || cl[1] == vb[a] || cl[2] == vb[a] ||
-//						ss[0] == vb[a] || ss[1] == vb[a]){
-//					
-//					return false;				
-//				
-//				}
-			
-			
-	
-		}
-		
-			for(int b = 0; b < vb.length; b++){
-				
-				
-				int total =+ vb[b];
-				
-				if(vb[b] < 0 || vb[b] > 100){
+
+			for(int a = 0; a < vb.length; a++){
+
+				if(vb[a] == va[a] || vb[a] == vd[a] || vb[a] == vc[a] || vb[a] == ve[a]){
+
 					
 					return false;
-				
+
 				}
-				
-				if(total % 10 >  9){
-				
-					return false;
-				
-				}
-			
+
 			}
-			
+
+			for(int b = 0; b < vb.length; b++){
+
+				if(vb[b] == -2057 || vb[b] == -2061 || vb[b] == -2058 || vb[b] == -2059 || vb[b] == -2060){
+					
+					
+					
+				}else if(vb[b] < 0 || vb[b] > 100){
+
+					return false;
+
+				}
+
+				if((dir == 0 || dir == 2) && vb[b] / 10 > loc / 10){
+
+					return false;
+
+				}
+
+			}
+
 			return true;
-						
+
 		}else
 			return false;
-		
+
 	}
-	
-	
-	
+
+
+
 	public static void findCoords(int s, int dir, int clas){
-		
+
 		if(clas == 0){
-			
+
 			bb[0] = s;
-			
+
 			if(dir == 0){
-				
+
 				bb[1] = s+1;
 				bb[2] = s+2;
 				bb[3] = s+3;
 				bb[4] = s+4;
-				
+
 			}else if(dir == 1){
-				
+
 				bb[1] = s+10;
 				bb[2] = s+20;
 				bb[3] = s+30;
 				bb[4] = s+40;
-				
+
 			}else if(dir == 2){
-				
+
 				bb[1] = s-1;
 				bb[2] = s-2;
 				bb[3] = s-3;
 				bb[4] = s-4;
-				
+
 			}else if(dir == 3){
-				
+
 				bb[1] = s-10;
 				bb[2] = s-20;
 				bb[3] = s-30;
 				bb[4] = s-40;
-				
+
 			}
-			
+
 		}else if(clas == 1){
-			
+
 			ca[0] = s;
-			
+
 			if(dir == 0){
-				
+
 				ca[1] = s+1;
 				ca[2] = s+2;
 				ca[3] = s+3;
-				
+
 			}else if(dir == 1){
-				
+
 				ca[1] = s+10;
 				ca[2] = s+20;
 				ca[3] = s+30;
-				
+
 			}else if(dir == 2){
-				
+
 				ca[1] = s-1;
 				ca[2] = s-2;
 				ca[3] = s-3;
-				
+
 			}else if(dir == 3){
-				
+
 				ca[1] = s-10;
 				ca[2] = s-20;
 				ca[3] = s-30;
-				
+
 			}
-			
+
 		}else if(clas == 2){
-			
+
 			dd[0] = s;
-			
+
 			if(dir == 0){
-				
+
 				dd[1] = s+1;
 				dd[2] = s+2;
-				
+
 			}else if(dir == 1){
-				
+
 				dd[1] = s+10;
 				dd[2] = s+20;
-				
+
 			}else if(dir == 2){
-				
+
 				dd[1] = s-1;
 				dd[2] = s-2;
-				
+
 			}else if(dir == 3){
-				
+
 				dd[1] = s-10;
 				dd[2] = s-20;
-				
+
 			}
-			
+
 		}else if(clas == 3){
-			
+
 			cl[0] = s;
-			
+
 			if(dir == 0){
-				
+
 				cl[1] = s+1;
 				cl[2] = s+2;
-				
+
 			}else if(dir == 1){
-				
+
 				cl[1] = s+10;
 				cl[2] = s+20;
-				
+
 			}else if(dir == 2){
-				
+
 				cl[1] = s-1;
 				cl[2] = s-2;
-				
+
 			}else if(dir == 3){
-				
+
 				cl[1] = s-10;
 				cl[2] = s-20;
-				
+
 			}
-			
+
 		}else if(clas == 4){
-			
+
 			ss[0] = s;
-			
+
 			if(dir == 0){
-				
+
 				ss[1] = s+1;
-				
+
 			}else if(dir == 1){
-				
+
 				ss[1] = s+10;
-				
+
 			}else if(dir == 2){
-				
+
 				ss[1] = s-1;
-				
+
 			}else if(dir == 3){
-				
+
 				ss[1] = s-10;
-				
+
 			}
-		
+
 		}
-	
+
 	}
 
 }
--- Battleship/src/game/Play.java
+++ Battleship/src/game/Play.java
@@ -3,33 +3,72 @@ package game;
 import java.util.Scanner;
 
 public class Play {
-	
+
 	public static Interface board = new Interface();
 	public static Builder build = new Builder();
-	
+
 	public static Scanner in = new Scanner(System.in);
-	
+
 	public static void main(String args[]){
-		
-		int coord, dir;
-		
-		coord = in.nextInt();
-		dir = in.nextInt();
-		
-		placeShip(coord, dir, 0);
-		
+
+		int coord, dir, col, row;
+		char direction;
+
 		board.printInterface();
-		
-	}
-	
-	public static void placeShip(int coord, int dir, int val){
-		
-		if(build.checkOverlap(coord, dir, val)){
-			
-			board.findCoords(coord, dir, val);
-			
+
+		do{
+
+		System.out.format("%nPlease enter the column you would like to place your battleship in:");
+
+		col = in.nextInt();
+
+		System.out.format("%nPlease enter the row you would like to place your battleship in:");
+
+		row = in.nextInt();
+
+		System.out.format("%nPlease enter the diretion you want your battleship facing."
+				+ "%n(U)p, (D)own, (L)eft, (R)ight%n");
+
+		direction = in.next().toUpperCase().charAt(0);
+
+		switch(direction){
+
+		case 85: dir = 3;
+				break;
+
+		case 68: dir = 1;
+				break;
+
+		case 76: dir = 2;
+				break;
+
 		}
-		
+
+		dir = in.nextInt();
+
+		coord = (row-1) * 10 + (col-1);
+
+		}while(placeShip(coord, dir, 0));
+
+		board.printInterface();
+
+	}
+
+	public static boolean placeShip(int coord, int dir, int val){
+
+		if(build.checkOverlap(coord, dir, val)){
+
+			board.findCoords(coord, dir, val);
+			return false;
+
+		}else{
+
+			System.out.format("That is not a valid placement for your ship. %nPlease try again.");
+			return true;
+
+		}
+
+
 	}
 
 }
