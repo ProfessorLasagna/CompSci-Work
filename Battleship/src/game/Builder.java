870502a5d44fa85b17c3f48d9cfd88033c2aea8b
999104001 <999104001@W870-2130.edu.cbe.ab.ca>
2017-05-03 13:49:46 -0600

    Dont even know anymore

--- Battleship/src/game/Builder.java
+++ Battleship/src/game/Builder.java
@@ -1,53 +1,33 @@
 package game;
 
+import java.util.ArrayList;
+
 public class Builder {
 
-	public static int[] bb = new int[5];
-	public static int[] ca = new int[5];
-	public static int[] dd = new int[5];
-	public static int[] cl = new int[5];
-	public static int[] ss = new int[5];
-	public static int[] vb = new int[5];
-	public static int[] va = new int[5];
-	public static int[] vc = new int[5];
-	public static int[] vd = new int[5];
-	public static int[] ve = new int[5];
+	static ArrayList<Integer> bb =new ArrayList<Integer>();
+	static ArrayList<Integer> ca =new ArrayList<Integer>();
+	static ArrayList<Integer> dd =new ArrayList<Integer>();
+	static ArrayList<Integer> cl =new ArrayList<Integer>();
+	static ArrayList<Integer> ss =new ArrayList<Integer>();
+	ArrayList<Integer> vb =new ArrayList<Integer>();
+	ArrayList<Integer> va =new ArrayList<Integer>();
+	ArrayList<Integer> vc =new ArrayList<Integer>();
+	ArrayList<Integer> vd =new ArrayList<Integer>();
+	ArrayList<Integer> ve =new ArrayList<Integer>();
 
 	public Builder() {
 
-		for(int a = 0; a < 5; a++){
-
-			bb[a] = -2057;
-
-		}
-
-		for(int a = 0; a < 5; a++){
-
-			ca[a] = -2058;
-
-		}
-
-		for(int a = 0; a < 5; a++){
-
-			dd[a] = -2059;
-
-		}
-
-		for(int a = 0; a < 5; a++){
-
-			cl[a] = -2060;
-
-		}
-
-		for(int a = 0; a < 5; a++){
-
-			ss[a] = -2061;
-
-		}
 
 	}
 
 	public boolean checkOverlap(int loc, int dir, int boat){
+
+		vb.clear();
+		va.clear();
+		vc.clear();
+		vd.clear();
+		ve.clear();
+
 
 		findCoords(loc, dir, boat);
 
@@ -60,6 +40,31 @@ public class Builder {
 			vd = bb;
 			ve = ca;
 
+			vb.add(ss.get(1));
+			vb.add(ss.get(1));
+			vb.add(ss.get(1));
+
+			va.add(-2);
+			va.add(-2);
+			va.add(-2);
+			va.add(-2);
+			va.add(-2);
+			vc.add(-3);
+			vc.add(-3);
+			vc.add(-3);
+			vc.add(-3);
+			vc.add(-3);
+			vd.add(-4);
+			vd.add(-4);
+			vd.add(-4);
+			vd.add(-4);
+			vd.add(-4);
+			ve.add(-5);
+			ve.add(-5);
+			ve.add(-5);
+			ve.add(-5);
+			ve.add(-5);
+
 		}else if(boat == 2){
 
 			boat = 3;
@@ -68,6 +73,29 @@ public class Builder {
 			vc = cl;
 			vd = bb;
 			ve = ca;
+
+			vb.add(0);
+			vb.add(0);
+			va.add(-2);
+			va.add(-2);
+			va.add(-2);
+			va.add(-2);
+			va.add(-2);
+			vc.add(-3);
+			vc.add(-3);
+			vc.add(-3);
+			vc.add(-3);
+			vc.add(-3);
+			vd.add(-4);
+			vd.add(-4);
+			vd.add(-4);
+			vd.add(-4);
+			vd.add(-4);
+			ve.add(-5);
+			ve.add(-5);
+			ve.add(-5);
+			ve.add(-5);
+			ve.add(-5);
 
 		}else if(boat == 3){
 
@@ -78,6 +106,29 @@ public class Builder {
 			vd = bb;
 			ve = ca;
 
+			vb.add(0);
+			vb.add(0);
+			va.add(-2);
+			va.add(-2);
+			va.add(-2);
+			va.add(-2);
+			va.add(-2);
+			vc.add(-3);
+			vc.add(-3);
+			vc.add(-3);
+			vc.add(-3);
+			vc.add(-3);
+			vd.add(-4);
+			vd.add(-4);
+			vd.add(-4);
+			vd.add(-4);
+			vd.add(-4);
+			ve.add(-5);
+			ve.add(-5);
+			ve.add(-5);
+			ve.add(-5);
+			ve.add(-5);
+
 		}else if(boat == 1){
 
 			boat = 4;
@@ -86,6 +137,28 @@ public class Builder {
 			vc = cl;
 			vd = bb;
 			ve = ss;
+
+			vb.add(0);
+			va.add(-2);
+			va.add(-2);
+			va.add(-2);
+			va.add(-2);
+			va.add(-2);
+			vc.add(-3);
+			vc.add(-3);
+			vc.add(-3);
+			vc.add(-3);
+			vc.add(-3);
+			vd.add(-4);
+			vd.add(-4);
+			vd.add(-4);
+			vd.add(-4);
+			vd.add(-4);
+			ve.add(-5);
+			ve.add(-5);
+			ve.add(-5);
+			ve.add(-5);
+			ve.add(-5);
 
 		}else if(boat == 0){
 
@@ -96,33 +169,53 @@ public class Builder {
 			vd = ss;
 			ve = ca;
 
+			va.add(-2);
+			va.add(-2);
+			va.add(-2);
+			va.add(-2);
+			va.add(-2);
+			vc.add(-3);
+			vc.add(-3);
+			vc.add(-3);
+			vc.add(-3);
+			vc.add(-3);
+			vd.add(-4);
+			vd.add(-4);
+			vd.add(-4);
+			vd.add(-4);
+			vd.add(-4);
+			ve.add(-5);
+			ve.add(-5);
+			ve.add(-5);
+			ve.add(-5);
+			ve.add(-5);
+
+
 		}
 
 		if(loc < 100 && loc >= 0){
 
-			for(int a = 0; a < 5; a++){
+			for(int a = 0; a < vb.size(); a++){
 
-				if(vb[a] == va[a] || vb[a] == vd[a] || vb[a] == vc[a] || vb[a] == ve[a]){
-					
+				if(vb.get(a) == va.get(a) || vb.get(a) == vd.get(a) || vb.get(a) == vc.get(a) || vb.get(a) == ve.get(a)){
+
+
+
 					return false;
 
 				}
 
 			}
 
-			for(int b = 0; b < vb.length; b++){
+			for(int b = 0; b < vb.size(); b++){
 
-				if(vb[b] == -2057 || vb[b] == -2061 || vb[b] == -2058 || vb[b] == -2059 || vb[b] == -2060){
-					
-					
-					
-				}else if(vb[b] < 0 || vb[b] > 100){
-
+				if(vb.get(b) < 0 || vb.get(b) > 100){
+System.out.println(b+" "+vb.get(b));
 					return false;
 
 				}
 
-				if((dir == 0 || dir == 2) && ((vb[b] - (vb[b] % 10)) / 10) > (loc - (loc % 10)) / 10 || ((vb[b] - (vb[b] % 10)) / 10) < (loc - (loc % 10)) / 10){
+				if((dir == 0 || dir == 2) && ((vb.get(b) - (vb.get(b) % 10)) / 10) != (loc - (loc % 10)) / 10){
 
 					return false;
 
@@ -143,139 +236,139 @@ public class Builder {
 
 		if(clas == 0){
 
-			bb[0] = s;
+			bb.add(s);
 
 			if(dir == 0){
 
-				bb[1] = s+1;
-				bb[2] = s+2;
-				bb[3] = s+3;
-				bb[4] = s+4;
+				bb.add(s+1);
+				bb.add(s+2);
+				bb.add(s+3);
+				bb.add(s+4);
 
 			}else if(dir == 1){
 
-				bb[1] = s+10;
-				bb[2] = s+20;
-				bb[3] = s+30;
-				bb[4] = s+40;
+				bb.add(s+10);
+				bb.add(s+20);
+				bb.add(s+30);
+				bb.add(s+40);
 
 			}else if(dir == 2){
 
-				bb[1] = s-1;
-				bb[2] = s-2;
-				bb[3] = s-3;
-				bb[4] = s-4;
+				bb.add(s-1);
+				bb.add(s-2);
+				bb.add(s-3);
+				bb.add(s-4);
 
 			}else if(dir == 3){
 
-				bb[1] = s-10;
-				bb[2] = s-20;
-				bb[3] = s-30;
-				bb[4] = s-40;
+				bb.add(s-10);
+				bb.add(s-20);
+				bb.add(s-30);
+				bb.add(s-40);
 
 			}
 
 		}else if(clas == 1){
 
-			ca[0] = s;
+			ca.add(s);
 
 			if(dir == 0){
 
-				ca[1] = s+1;
-				ca[2] = s+2;
-				ca[3] = s+3;
+				ca.add(s+1);
+				ca.add(s+2);
+				ca.add(s+3);
 
 			}else if(dir == 1){
 
-				ca[1] = s+10;
-				ca[2] = s+20;
-				ca[3] = s+30;
+				ca.add(s+10);
+				ca.add(s+20);
+				ca.add(s+30);
 
 			}else if(dir == 2){
 
-				ca[1] = s-1;
-				ca[2] = s-2;
-				ca[3] = s-3;
+				ca.add(s-1);
+				ca.add(s-2);
+				ca.add(s-3);
 
 			}else if(dir == 3){
 
-				ca[1] = s-10;
-				ca[2] = s-20;
-				ca[3] = s-30;
+				ca.add(s-10);
+				ca.add(s-20);
+				ca.add(s-30);
 
 			}
 
 		}else if(clas == 2){
 
-			dd[0] = s;
+			dd.add(s);
 
 			if(dir == 0){
 
-				dd[1] = s+1;
-				dd[2] = s+2;
+				dd.add(s+1);
+				dd.add(s+2);
 
 			}else if(dir == 1){
 
-				dd[1] = s+10;
-				dd[2] = s+20;
+				dd.add(s+10);
+				dd.add(s+20);
 
 			}else if(dir == 2){
 
-				dd[1] = s-1;
-				dd[2] = s-2;
+				dd.add(s-1);
+				dd.add(s-1);
 
 			}else if(dir == 3){
 
-				dd[1] = s-10;
-				dd[2] = s-20;
+				dd.add(s-10);
+				dd.add(s-20);
 
 			}
 
 		}else if(clas == 3){
 
-			cl[0] = s;
+			cl.add(s);
 
 			if(dir == 0){
 
-				cl[1] = s+1;
-				cl[2] = s+2;
+				cl.add(s+1);
+				cl.add(s+2);
 
 			}else if(dir == 1){
 
-				cl[1] = s+10;
-				cl[2] = s+20;
+				cl.add(s+10);
+				cl.add(s+20);
 
 			}else if(dir == 2){
 
-				cl[1] = s-1;
-				cl[2] = s-2;
+				cl.add(s-1);
+				cl.add(s-2);
 
 			}else if(dir == 3){
 
-				cl[1] = s-10;
-				cl[2] = s-20;
+				cl.add(s-10);
+				cl.add(s-20);
 
 			}
 
 		}else if(clas == 4){
 
-			ss[0] = s;
+			ss.add(s);
 
 			if(dir == 0){
 
-				ss[1] = s+1;
+				ss.add(s+1);
 
 			}else if(dir == 1){
 
-				ss[1] = s+10;
+				ss.add(s+10);
 
 			}else if(dir == 2){
 
-				ss[1] = s-1;
+				ss.add(s-1);
 
 			}else if(dir == 3){
 
-				ss[1] = s-10;
+				ss.add(s-10);
 
 			}
 
