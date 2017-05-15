3624bdbc705c622cfcbf64fb9bded381623f92c1
999104001 <999104001@W870-2130.edu.cbe.ab.ca>
2017-05-15 13:49:19 -0600

    !!!

---
+++ Recursion/src/factorials/calc/Calc.java
@@ -0,0 +1,20 @@
+package factorials.calc;
+
+public class Calc {
+
+	public static void main(String[] args) {
+		
+				System.out.println(fact(28));
+
+	}
+
+	public static int fact(int n){
+		
+		if(n == 0)
+			return 1;
+		
+		return (n * fact(n-1));
+		
+	}
+	
+}
